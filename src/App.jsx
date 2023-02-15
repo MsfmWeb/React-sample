e6629de4c77ddbaf2c887f4a94148dbfdbcbecde
MsfmWeb <115722476+MsfmWeb@users.noreply.github.com>
2023-02-15 13:54:40 +0900

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+// import React from "react";
+import React, { useEffect, useState } from "react";
 import Message from "./components/message";
 
 const App = () => {
@@ -14,11 +15,24 @@ const App = () => {
   // 可変の状態
   // ⇒stateが変更されると再レンダリングされる
   // 動的に値が変化するときに用いる
+  useEffect(() => {
+    if (Num > 0) {
+      if (Num % 3 === 0) {
+        FaseShowFlag || setFaseShowFlag(true);
+      } else {
+        FaseShowFlag && setFaseShowFlag(false);
+      }
+    }
+  }, [Num]);
+  // ステートが最初から処理が走る
+  // UseEffectを使い、[]の中の条件で処理のタイミングを制御可能
+  // 第二引数を[]だけにすると最初の1回だけ実行という処理に変更できる
 
   const onClickSwitchShowFlag = () => {
     setFaseShowFlag(!FaseShowFlag);
   };
-  const [FaseShowFlag, setFaseShowFlag] = useState(true);
+  const [FaseShowFlag, setFaseShowFlag] = useState(false);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
@@ -26,8 +40,14 @@ const App = () => {
       <Message color="pink">元気です</Message>
       <Message color="blue">元気です</Message>
       <button onClick={onClickCountup}>カウントアップ</button>
-      <button>on/off</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{Num}</p>
+      {/* && ⇒ 左辺がtrueなら右辺を出力する */}
+      {FaseShowFlag && <p>ON</p>}
+
+      {/* || ⇒ 左辺がfalseなら右辺を出力する */}
+      {/* {FaseShowFlag || <p>OFF</p>} */}
     </>
   );
 };
